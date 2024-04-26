12dae76c5cdbd53ca2759c8b770ac06bf9120236
knilesh9 <neutron_stays@protonmail.com>
2024-04-26 21:42:38 +0530

    Done with prisma and generics

--- medium-rerun/src/index.ts
+++ medium-rerun/src/index.ts
@@ -1,8 +1,19 @@
 import { Hono } from 'hono'
+import { User } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{  
+  Bindings: {
+  DATABASE_URL: string,
+  JWT_SECRET: string
+}}>()
 
 app.post('/api/v1/user/signup', (c) => {
+
+  const prima = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
   return c.text('Hello Hono!')
 })
 
