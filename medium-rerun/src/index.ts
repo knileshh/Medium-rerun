5bf34d119ca77aa9d39c9545baccc6df2dc14bcd
knilesh9 <neutron_stays@protonmail.com>
2024-04-26 20:50:07 +0530

    add routes

--- medium-rerun/src/index.ts
+++ medium-rerun/src/index.ts
@@ -2,7 +2,27 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/singin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
   return c.text('Hello Hono!')
 })
 
